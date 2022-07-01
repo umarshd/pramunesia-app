71622b8eb755eca744d717e228baf138cc099f89
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 01:49:48 +0700

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -36,6 +36,21 @@ $(function () {
   });
 });
 
+$(function () {
+  var dtToday = new Date();
+
+  var month = dtToday.getMonth() + 1;
+  var day = dtToday.getDate();
+  var year = dtToday.getFullYear();
+  if (month < 10) month = "0" + month.toString();
+  if (day < 10) day = "0" + day.toString();
+
+  var minDate = year + "-" + month + "-" + day;
+
+  $("#txtDate").attr("min", minDate);
+  $("#txtDate2").attr("min", minDate);
+});
+
 // fungsi delete
 function confirmToDelete(el) {
   $("#delete-button").attr("href", el.dataset.href);

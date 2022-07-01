1a035edfff22f15413142c8a2225370560e61ba3
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-01 10:04:23 +0700

    Create login-wisatawan.php

---
+++ app/Views/auth/login-wisatawan.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css">
+
+</head>
+
+<body>
+  <div class="container">
+    <div class=" row h-100vh">
+      <div class="col-lg-6 ">
+        <div class="d-flex align-item-center">
+          <img class="mx-auto mt-auto" src="<?= base_url() ?>/assets/images/logo-big.png" alt="">
+        </div>
+      </div>
+      <div class="col-lg-6 bg-firefly ">
+        <div class="text-center mb-5 text-white">
+          <h6>Masuk</h6>
+          <h3 class="my-4">Selamat Datang</h3>
+          <p>Masuk Sekarang dan Mulai perjalanan Anda bersama kami</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>

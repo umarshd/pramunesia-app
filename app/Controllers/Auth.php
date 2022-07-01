088ae865dc77905a1bac2f05c87571fdcdc2e696
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 01:49:11 +0700

    Update Auth.php

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,14 +3,17 @@
 namespace App\Controllers;
 
 use App\Models\WisatawanModel;
+use App\Models\PemanduModel;
 
 class Auth extends BaseController
 {
   protected $WisatawanModel;
+  protected $PemanduModel;
 
   public function __construct()
   {
     $this->WisatawanModel = new WisatawanModel();
+    $this->PemanduModel = new PemanduModel();
   }
 
   public function loginWisatawan()
@@ -28,7 +31,7 @@ class Auth extends BaseController
       'email' => [
         'rules' => 'required',
         'errors' => [
-          'required' => 'Email harus diisi',
+          'required' => 'Email harus diisi'
         ]
       ],
       'password' => [
@@ -44,7 +47,28 @@ class Auth extends BaseController
       return redirect()->back()->withInput();
     }
 
-    return 'lolos validasi';
+    $email = $this->request->getVar('email');
+    $password = $this->request->getVar('password');
+    $cekEmail = $this->WisatawanModel->where('email', $email)->first();
+
+    if (!$cekEmail) {
+      session()->setFlashdata('error', 'email atau password salah');
+      return redirect()->back()->withInput();
+    }
+
+    $passwordVerif = password_verify($password, $cekEmail['password']);
+
+    if (!$passwordVerif) {
+      session()->setFlashdata('error', 'email atau password salah');
+      return redirect()->back()->withInput();
+    }
+
+    session()->set([
+      'is_login_wisatawan' => true,
+      'wisatawan_id' => $cekEmail['id']
+    ]);
+
+    return redirect()->to('/wisatawan');
   }
 
   public function prosesRegistrasiWisatawan()
@@ -102,5 +126,136 @@ class Auth extends BaseController
     $this->WisatawanModel->insert($data);
     session()->setFlashdata('success', 'Berhasil registrasi');
     return redirect()->to('/wisatawan/login');
+  }
+
+  public function loginPemandu()
+  {
+    return view('auth/login-pemandu');
+  }
+  public function registrasiPemandu()
+  {
+    return view('auth/regis-pemandu');
+  }
+  public function prosesLoginPemandu()
+  {
+    $rules = $this->validate([
+      'email' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Email harus diisi'
+        ]
+      ],
+      'password' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Password harus diisi'
+        ]
+      ],
+    ]);
+
+    if (!$rules) {
+      session()->setFlashdata('error', $this->validator->listErrors());
+      return redirect()->back()->withInput();
+    }
+
+    $email = $this->request->getVar('email');
+    $password = $this->request->getVar('password');
+    $cekEmail = $this->PemanduModel->where('email', $email)->first();
+
+    if (!$cekEmail) {
+      session()->setFlashdata('error', 'email atau password salah');
+      return redirect()->back()->withInput();
+    }
+
+    $passwordVerif = password_verify($password, $cekEmail['password']);
+
+    if (!$passwordVerif) {
+      session()->setFlashdata('error', 'email atau password salah');
+      return redirect()->back()->withInput();
+    }
+
+    session()->set([
+      'is_login_pemandu' => true,
+      'pemandu_id' => $cekEmail['id']
+    ]);
+
+    return redirect()->to('/pemandu');
+  }
+  public function prosesRegistrasiPemandu()
+  {
+    $rules = $this->validate([
+      'noKta' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Nomer Kta harus diisi'
+        ]
+      ],
+      'nama' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Nama harus diisi'
+        ]
+      ],
+      'email' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Email harus diisi',
+        ]
+      ],
+      'telepon' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Nomer Telepon harus diisi'
+        ]
+      ],
+      'alamat' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Alamat harus diisi'
+        ]
+      ],
+      'jenis' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'jenis akun harus diisi'
+        ]
+      ],
+      'password' => [
+        'rules' => 'required',
+        'errors' => [
+          'required' => 'Password harus diisi'
+        ]
+      ],
+    ]);
+
+    if (!$rules) {
+      session()->setFlashdata('error', $this->validator->listErrors());
+      return redirect()->back()->withInput();
+    }
+
+    $password = $this->request->getVar('password');
+    $passwordVerif = $this->request->getVar('passwordVerif');
+
+    if ($password != $passwordVerif) {
+      session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
+      return redirect()->back()->withInput();
+    }
+
+    $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
+    $data = [
+      'noKta' => $this->request->getVar('noKta'),
+      'nama' => $this->request->getVar('nama'),
+      'email' => $this->request->getVar('email'),
+      'telepon' => $this->request->getVar('telepon'),
+      'alamat' => $this->request->getVar('alamat'),
+      'jenis' => $this->request->getVar('jenis'),
+      'password' => $passwordHas,
+      'image' => 'default.png'
+    ];
+
+    $this->PemanduModel->insert($data);
+    session()->setFlashdata('success', 'Berhasil registrasi');
+    return redirect()->to('/pemandu/login');
   }
 }

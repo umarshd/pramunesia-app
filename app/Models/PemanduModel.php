ce7f691cbf1248391fe3ed46c3bef7b118cfac0c
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 01:49:19 +0700

    Create PemanduModel.php

---
+++ app/Models/PemanduModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PemanduModel extends Model
+{
+  protected $table = 'pemandu';
+  protected $primaryKey = 'id';
+  protected $returnType = 'array';
+  protected $allowedFields = [
+    'noKta',
+    'nama',
+    'email',
+    'telepon',
+    'password',
+    'image',
+    'alamat',
+    'jenis',
+  ];
+}

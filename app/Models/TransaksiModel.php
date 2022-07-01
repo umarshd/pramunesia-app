ddc55f0294b77b3794f5deb01b6bc5e3c8e4f274
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 01:49:23 +0700

    Create TransaksiModel.php

---
+++ app/Models/TransaksiModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TransaksiModel extends Model
+{
+  protected $table = 'transaksi';
+  protected $primaryKey = 'id';
+  protected $returnType = 'array';
+  protected $allowedFields = [
+    'tanggal_pemesanan',
+    'tanggal_keberangkatan',
+    'tanggal_berakhir',
+    'status',
+    'kota_id',
+    'pemandu_id',
+    'wisatawan_id',
+  ];
+}

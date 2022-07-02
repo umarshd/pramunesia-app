1593967ccf881663ff1c0212ab3ef711e0a54804
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 19:44:12 +0700

    Update tambah.php

--- app/Views/admin/destinasi/tambah.php
+++ app/Views/admin/destinasi/tambah.php
@@ -16,19 +16,10 @@
   </div><!-- End Page Title -->
 
   <section class="section dashboard">
-    <div class="row">
-
-      <div class="col-lg-6">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
         <div class="card p-3">
-          <div class="row justify-content-center">
-            <div class="col-lg-6">
-              <h5>Tambah Destinasi</h5>
-            </div>
-            <div class="col-lg-6 text-end">
-              <a href="<?= base_url() ?>/admin/kota" class="btn btn-primary btn-sm">kembali</a>
-            </div>
-          </div>
-
+          <h5 class="fw-bold text-2">Tambah Destinasi</h5>
           <?php if (session()->get('error')) : ?>
             <div class="alert alert-danger my-2" role="alert">
               <?= session()->get('error') ?>
@@ -43,26 +34,32 @@
               <label class="form-label">Image</label>
               <input type="file" name="file" class="form-control" accept="image/*">
             </div>
-            <div class="form-group py-2">
-              <label class="form-label">Rekomendasi</label>
-              <select name="rekomendasi" class="form-select">
-                <option disabled selected>Pilih Status</option>
-                <option value="ya">Ya</option>
-                <option value="no">No</option>
-              </select>
-            </div>
-            <div class="form-group py-2">
-              <label class="form-label">Kota</label>
-              <select name="kota_id" class="form-select">
-                <option disabled selected>Pilih Kota</option>
-                <?php foreach ($dataKota as $kota) : ?>
-                  <option value="<?= $kota['id'] ?>"><?= $kota['nama'] ?></option>
-                <?php endforeach ?>
-              </select>
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="form-group py-2">
+                  <label class="form-label">Rekomendasi</label>
+                  <select name="rekomendasi" class="form-select">
+                    <option disabled selected>Pilih Status</option>
+                    <option value="ya">Ya</option>
+                    <option value="tidak">Tidak</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="form-group py-2">
+                  <label class="form-label">Kota</label>
+                  <select name="kota_id" class="form-select">
+                    <option disabled selected>Pilih Kota</option>
+                    <?php foreach ($dataKota as $kota) : ?>
+                      <option value="<?= $kota['id'] ?>"><?= $kota['nama'] ?></option>
+                    <?php endforeach ?>
+                  </select>
+                </div>
+              </div>
             </div>
 
             <div class="py-3 text-end">
-              <button class="btn btn-primary" type="submit">Kirim</button>
+              <button class="btn btn-custom-3" type="submit">Kirim</button>
             </div>
           </form>
         </div>

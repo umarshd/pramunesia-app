3a6d4ee69cebd74c8064496ed847128f2cb7f038
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 12:42:29 +0700

    Create index.php

---
+++ app/Views/pemandu/kegiatan/index.php
@@ -0,0 +1,43 @@
+<?= $this->extend('layouts/pemandu_layout') ?>
+<?= $this->section('content') ?>
+<div class="container my-3" style="min-height: 73vh ;">
+  <div class="row justify-content-center">
+    <div class="col-lg-12">
+      <div class="card p-3 bg-custom-2 border-radius-10 shadow-custom-2">
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <h4 class="text-white my-3">Dokumentasi Kegiatanmu</h4>
+          </div>
+          <div class="col-lg-6 text-end">
+            <a href="" class="btn btn-custom-3 btn-sm">Tambah Kegiatan</a>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?= $this->endSection() ?>

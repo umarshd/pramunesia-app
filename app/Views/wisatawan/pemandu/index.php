9e3536807e913dc6de153323020e5663fc6f8929
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 12:43:22 +0700

    Update index.php

--- app/Views/wisatawan/pemandu/index.php
+++ app/Views/wisatawan/pemandu/index.php
@@ -19,6 +19,41 @@
       </div>
     </div>
 
+    <div class="col-lg-12 mt-3">
+      <div class="card p-3 bg-custom-2 border-radius-10 shadow-custom-2">
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <h4 class="text-white my-3">Dokumentasi</h4>
+          </div>
+
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mt-3">
+            <div class=" card border-0 border-radius-10 p-4">
+              Hello
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
 
   </div>
 </div>

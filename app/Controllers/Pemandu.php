34c02a301349019ade944b222247a1aad185621f
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-07-02 08:23:17 +0700

    Create Pemandu.php

---
+++ app/Controllers/Pemandu.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\PemanduModel;
+use App\Models\CustomModel;
+use CodeIgniter\Files\Exceptions\FileNotFoundException;
+
+class Pemandu extends BaseController
+{
+  protected $PemanduModel;
+  protected $CustomModel;
+
+  public function __construct()
+  {
+    $this->PemanduModel = new PemanduModel();
+    $this->CustomModel = new CustomModel();
+  }
+
+  public function index()
+  {
+    $data = [
+      'jadwalMemandu' => $this->CustomModel->jadwalMemanduTerbaru(session()->get('pemandu_id'))
+    ];
+    return view('pemandu/index', $data);
+  }
+
+  public function profile()
+  {
+    $data = [
+      'pemandu' => $this->PemanduModel->where('id', session()->get('pemandu_id'))->first()
+    ];
+
+    return view('pemandu/profile/index');
+  }
+
+  public function editProfile($id = null)
+  {
+    $data = [
+      'pemandu'  => $this->PemanduModel->where('id', $id)->first()
+    ];
+
+    return view('pemandu/profile/edit');
+  }
+}
